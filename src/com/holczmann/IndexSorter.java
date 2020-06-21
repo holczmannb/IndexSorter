4289dbaeb86df7ddcc512d15e7aed895a6228fbe
holczmannb <31094585+holczmannb@users.noreply.github.com>
2020-06-21 12:50:52 +0200

    f

--- src/com/holczmann/IndexSorter.java
+++ src/com/holczmann/IndexSorter.java
@@ -35,7 +35,7 @@ public class IndexSorter {
 		}
 				
 		// Create helper array. Size is the maximal number from the input.
-		// Use the number form input array as index and set true by this index in the helper array. 
+		// Use the number from input array as index and set true by this index in the helper array. 
 		boolean[] b = new boolean[maxItem + 1];
 		for (int i = 0; i < a.length; i++) {
 			b[a[i]] = true;
@@ -58,7 +58,7 @@ public class IndexSorter {
 	}
 	
 	/**
-	 * Fill array with random unique numbers
+	 * Fill array with random distinct numbers
 	 * @param a: array as in/out parameter
 	 * @param range: interval of natural numeric number 
 	 */
@@ -84,54 +84,73 @@ public class IndexSorter {
 
 	/**
 	 * IndexSorter vs QuickSorter test application
-	 * @param args: size of the array as int; range of sortable number as int 
+	 * @param args: number of samples to be generated as int;
+	 * 				size of the array as int;
+	 * 				range of sortable numbers as int;  
+	 * 				print statistic only as boolean
 	 */
 	public static void main(String[] args) {
 		// Default size, range and print the sample
-		int size = 1000;
-		int range = 10000;
-		boolean statisticOnly = false;
+		int size = 100;
+		int range = 1000;
+		int numberOfSamples = 10;
+		boolean statisticOnly = true;
+		int quickWin = 0;
+		int indexWin = 0;
 		
 		// Read the input parameters
 		if (args.length > 0) {
 		    try {
-		        size = Integer.parseInt(args[0]);
-		        range = Integer.parseInt(args[1]);
-		        statisticOnly = Boolean.parseBoolean(args[2]);
+		    	numberOfSamples = Integer.parseInt(args[0]);
+		        size = Integer.parseInt(args[1]);
+		        range = Integer.parseInt(args[2]);
+		        statisticOnly = Boolean.parseBoolean(args[3]);
 		    } catch (NumberFormatException e) {
-		        System.err.println("Argument" + args[0] + " must be an integer.");
-		        System.err.println("Argument" + args[1] + " must be an integer.");
-		        System.err.println("Argument" + args[2] + " must be an boolean.");
+		        System.out.println("Argument" + args[0] + " must be an integer.");
+		        System.out.println("Argument" + args[1] + " must be an integer.");
+		        System.out.println("Argument" + args[2] + " must be an integer.");
+		        System.out.println("Argument" + args[3] + " must be an boolean.");
 		        System.exit(1);        
-		    }
+		    } catch (Exception e) {
+		        System.out.println("Argument: number of samples to be generated as int;	size of the array as int; range of sortable numbers as int;	print statistic only as boolean");
+		    } 		
 		}
 		
+		if(range < size) {
+			System.out.println("Size of the array must be less then the number range!");
+			System.exit(1); 
+		}
 		
 		// Sample Generation
-		int[] originalSample = new int[size];
-		IndexSorter.generateSample(originalSample, range);
-		System.out.println("Sample generation started...");
-		System.out.println("Sample generated (size:" + size + " / intervall: " + range + ") ");
-		if (!statisticOnly) System.out.println(Arrays.toString(originalSample));
-		
-		// QuickSort
-		int[] a = originalSample.clone();
-		
-		long runTimeQuick = IndexSorter.guicksort(a);
-		
-		System.out.println("QuickSort (" + runTimeQuick +"ns) ");
-		if (!statisticOnly) System.out.println(Arrays.toString(a));
-				
-	
-		// IndexSort
-		a = originalSample.clone();
-		
-		long runTimeIndex = IndexSorter.indexsort(a);
-		
-		System.out.println("IndexSort (" + runTimeIndex +"ns) ");
-		if (!statisticOnly) System.out.println(Arrays.toString(a));
-		long percentage = ((runTimeQuick - runTimeIndex) * 100) / runTimeQuick;
-		System.out.println("Runtime reduction (QuickSort - IndexSort / QuickSort) : " + percentage + "%");
-	}
+		for (int count = 0; count < numberOfSamples; count++) {
+			int[] originalSample = new int[size];
+			IndexSorter.generateSample(originalSample, range);
+			System.out.println("Sample generated (size:" + size + " / intervall: " + range + ") ");
+			if (!statisticOnly)
+				System.out.println(Arrays.toString(originalSample));
 
+			// QuickSort
+			int[] a = originalSample.clone();
+
+			long runTimeQuick = IndexSorter.guicksort(a);
+
+			System.out.println("QuickSort (" + runTimeQuick + "ns) ");
+			if (!statisticOnly)
+				System.out.println(Arrays.toString(a));
+
+			// IndexSort
+			a = originalSample.clone();
+
+			long runTimeIndex = IndexSorter.indexsort(a);
+
+			System.out.println("IndexSort (" + runTimeIndex + "ns) ");
+			if (!statisticOnly)
+				System.out.println(Arrays.toString(a));
+			long percentage = ((runTimeQuick - runTimeIndex) * 100) / runTimeQuick;
+			System.out.println("Runtime reduction (QuickSort - IndexSort / QuickSort) : " + percentage + "%");
+			
+			if (runTimeQuick > runTimeIndex) indexWin++; else quickWin++;		
+		}// for
+		System.out.println("Wins QuickSort:" + quickWin + " IndexWin:" + indexWin);
+	}// main
 }
